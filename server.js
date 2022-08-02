97b873b5a569cb2fffba8a74e412725702732e49
Yaser Odat <yaserodat@outlook.com>
2022-08-02 21:41:41 +0300

    weather data added

--- server.js
+++ server.js
@@ -2,14 +2,55 @@
 
 const express = require('express');
 require('dotenv').config();
+const cors =  require('cors');
 
 const app = express();
-4
-const port =  process.env.PORT || 3001;
+app.use(cors()); 
+const port =  3001;
 
-app.get('/api', (req, res) => {
-    res.send('Message from the server')
+const weatherData = require ('./data/weather.json');
+// Routes
+app.get('/photo', handleHome);
+
+app.get('/weather', (req, res) =>{
+    let searchQuery = req.query.searchQuery;
+    const city = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+   
+    try {
+        const weatherArr = city.data.map(item => new Forecast(item))
+
+         res.status(200).send(weatherArr)
+
+    } catch (err) {
+
+        handleError (err, res)
+    }
+   
+    
+
+
 })
+
+class Forecast {
+    constructor(day) {
+        this.date = day.valid_date;
+        this.description = day.weather.description;
+
+    }
+}
+
+// Functions:
+
+function handleHome(req, res){
+    const photoName= req.query.photoName
+    res.send('Test2!')
+}
+
+
+function handleError (error, res) {
+    res.status(500).send('Sorry, the city you are looking for is not found.')
+}
+
 app.listen (port, ()=> {
-    console.log(`Listening to port ${port}`)
+    console.log(`Listening to porttt ${port}`)
 })
