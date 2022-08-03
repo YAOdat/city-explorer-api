d2e131186440e974ec7c9a623345198b94ba6a05
Yaser Odat <yaserodat@outlook.com>
2022-08-03 22:23:31 +0300

    added movie feature

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const axios = require('axios');
 const {handleWeather} = require('./modules/weather');
 
-const port = 3001
+const port = 4000
 const app = express();
 app.use(cors());
 
@@ -12,6 +12,18 @@ app.use(cors());
 
 app.get('/weather', handleWeather)
 
+app.get('/movies', async (req, res) => {
+    const {searchQuery, lat, lon} = req.query;
+    
+    const movieArr = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
+  
+    try {
+      const movieData = movieArr.data.results.map(item => new Movie(item));
+      res.status(200).send(movieData)
+    } catch (error) {
+      errorHandler(error, res)
+    }
+  })
 
 app.get('*', (req, res) => {res.status(404).send('The page you are looking for is not found!')})
 
@@ -20,6 +32,19 @@ function errorHandler(error, res) {
 }
 
 
-app.listen(3001, () => {
+class Movie {
+    constructor(movie) {
+      this.title = movie.title;
+      this.overview = movie.overview;
+      this.average_votes = movie.vote_average
+      this.total_votes = movie.vote_count
+      this.image_url = movie.poster_path;
+      this.popularity = movie.popularity
+      this.released_on = movie.release_date;
+    }
+  }
+
+
+app.listen(port, () => {
   console.log('Listening to port:', port)
 })
