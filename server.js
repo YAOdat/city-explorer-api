e281114c1757f6768bfa588b8d8bf47e3e35861e
Yaser Odat <yaserodat@outlook.com>
2022-08-05 16:02:24 +0300

    added modules

--- server.js
+++ server.js
@@ -1,50 +1,28 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const axios = require('axios');
+
 const {handleWeather} = require('./modules/weather');
+const {handleMovie} = require('./modules/movies');
 
 const port = process.env.PORT || 4000
+
 const app = express();
 app.use(cors());
 
-// const weatherData = require('./data/weather.json');
 
-app.get('/weather', handleWeather)
+app.get('/weather', handleWeather);
+app.get('/movies', handleMovie);
 
-app.get('/movies', async (req, res) => {
-    const {searchQuery, lat, lon} = req.query;
-    
-    const movieArr = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
-  
-    try {
-      const movieData = movieArr.data.results.map(item => new Movie(item));
-      res.status(200).send(movieData)
-    } catch (error) {
-      errorHandler(error, res)
-    }
-  })
 
-app.get('*', (req, res) => {res.status(404).send('The page you are looking for is not found!')})
+app.get('*', (req, res) => {res.status(404).send('The page you are trying to access does not exist!')})
 
 function errorHandler(error, res) {
-  res.status(500).send({error: 'Something went wrong'})
+  res.status(500).send({error: 'Something went wrong!'})
 }
 
+// test
 
-class Movie {
-    constructor(movie) {
-      this.title = movie.title;
-      this.overview = movie.overview;
-      this.average_votes = movie.vote_average
-      this.total_votes = movie.vote_count
-      this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      this.popularity = movie.popularity
-      this.released_on = movie.release_date;
-    }
-  }
-
-// test 
-app.listen(port, () => {
-  console.log('Listening to port:', port)
+app.listen(process.env.PORT, () => {
+  console.log('listening to port', port)
 })
