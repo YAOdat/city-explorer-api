7ef01ac61a7e423d83eeb2b1e349bb1d39e42ef3
Yaser Odat <yaserodat@outlook.com>
2022-08-06 23:31:00 +0300

    cache added

--- modules/weather.js
+++ modules/weather.js
@@ -1,11 +1,18 @@
 const axios = require('axios');
-
+const weatherCache = {};
 async function handleWeather(req, res) {
   const searchQuery = req.query.searchQuery;
   const lat = req.query.lat;
   const lon = req.query.lon;
-  // WEATHER_API_KEY
-  // const cityArr = weatherData.find(item => item.city_name.toLowerCase() === searchQuery.toLowerCase())
+  
+
+if (weatherCache !== undefined){
+
+} else {
+  
+}
+
+
   const cityArr = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
   console.log(cityArr.data)
   try {
